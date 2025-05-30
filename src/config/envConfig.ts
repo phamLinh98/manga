9312c4225dd043fcde5611332cc5adf865454d9d
phamLinh98 <linhthusinh98@gmail.com>
2025-05-31 02:26:34 +0900

    Update API URLs to use mock data from external API

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -1,8 +1,8 @@
 export const envConfig = {
   API_URL_CHAPTER:
     import.meta.env.API_URL_CHAPTER ||
-    "http://localhost:3000/chapter",
+    "https://my.api.mockaroo.com/chapter?key=ed51a920",
   API_URL_LIST:
     import.meta.env.API_URL_LIST ||
-    "http://localhost:3000/list",
+    "https://my.api.mockaroo.com/list?key=ed51a920",
 };
